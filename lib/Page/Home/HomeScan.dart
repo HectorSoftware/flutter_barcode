ed569a04119d5e7b023aea951bc377d435abcb82
SanvicenteCesar <cesarsvp17@gmail.com>
2019-04-18 11:18:18 -0400

    Services Connection

--- lib/Page/Home/HomeScan.dart
+++ lib/Page/Home/HomeScan.dart
@@ -5,6 +5,7 @@ import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_barcode/Page/Capture/CaptureCode.dart';
 import 'package:flutter_barcode/Page/Cards/Cards.dart';
+import 'package:flutter_barcode/api/Services.dart';
 
 
 class HomePagescan extends StatefulWidget {
@@ -19,13 +20,23 @@ class HomePagescan extends StatefulWidget {
 
 class HomePagescanState extends State<HomePagescan>  with SingleTickerProviderStateMixin{
 
+  Services Servicios = new Services();
   TabController _controller;
 
   @override
   void initState() {
+    PeticionGrpc();
     _controller = TabController(length: 2, vsync: this );
     super.initState();
   }
+
+  void PeticionGrpc() async{
+
+    Servicios.ConnectionTest('192.168.1.14',3000);
+    Servicios.TraerInvoice('');
+    //Servicios.CloseTest();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
